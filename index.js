7ecf01be6280fd1114bf754e54e385f582565032
steven-rr <steverelativity@gmail.com>
2021-09-18 02:23:09 -0400

    sending api from server to client and back.

--- index.js
+++ index.js
@@ -4,3 +4,16 @@ const app = express()
 app.listen(3000, () => console.log('listening at 3000'))
 
 app.use(express.static('public'))
+app.use(express.json({limit : '1mb'}))
+
+app.post('/api', (request, response) =>
+{
+    console.log("I got a request!")
+    console.log(request.body);
+    const data = request.body
+    response.json({
+        status: 'success',
+        lat: data.latitude,
+        lon: data.longitude 
+    })
+})
