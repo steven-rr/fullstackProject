57f3bdffb93d5d80fb680decdfffa28c38dbe9a8
steven-rr <steverelativity@gmail.com>
2021-09-19 02:32:58 -0400

    connecting front end to backend using axios

---
+++ client/src/axios.js
@@ -0,0 +1,8 @@
+// import Axios from "axios";
+
+
+// export const axios = Axios.create({
+
+//     baseURL: "http://localhost:3001",
+//     timeout: 3000
+// })
--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 require('dotenv').config();
 
 const app = express()
-app.listen(3000, () => console.log('listening at 3000'))
+app.listen(3001, () => console.log('listening at 3001'))
 
 app.use(express.static('public'))
 app.use(express.json({limit : '1mb'}))
@@ -20,12 +20,25 @@ app.post('/api', (request, response) =>
     })
 })
 
-app.get('/weather', async (request, response) => {
+app.get('/api/weather', async (request, response) => {
     const lat = 28.4;
     const lon = -81.4;    
     const api_url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.OPEN_WEATHER_API_KEY}`
     const fetch_response = await fetch(api_url)
     const json = await fetch_response.json()
+    response.json(json)
+    console.log(json)
+
+})
+
+app.get('/api/out', async (request, response) => {
+    const lat = 28.4;
+    const lon = -81.4;    
+    response.json({
+        status: 'success',
+        lat: lat,
+        lon: lon 
+    })
     response.json(json)
     console.log(json)
 
