12ee7fbb3e0b9cdbf8e6ca70c0ba55f1be001b7c
steven-rr <steverelativity@gmail.com>
2021-09-20 22:22:52 -0400

    in progress. bug where the state is one step behind..

--- client/src/pages/Form.js
+++ client/src/pages/Form.js
@@ -1,14 +1,37 @@
-import React from 'react'
+import React,  { useState } from 'react'
 import FormCSS from "./Form.module.css"
 
 const Form = () => {
+
+    const [values, setValues] = useState({username: '', password: '', email: '' })
+
+    const handleChange = e => {
+        const {name, value} = e.target;
+        // if (name === "Username")
+        // {
+            // console.log(value)
+            // console.log(values.username)
+            // setValues( currentValues =>{
+            //     console.log(value)
+            //     return { ...currentValues, username: value}
+            // })
+            setValues({...values, username: value})
+        // }
+        console.log(values.username)
+    }
+
     return (
         <div className={FormCSS.formContainer}>
             <div className= {FormCSS.textStyle}>Create an Account, Steven! </div>
             <form className= {FormCSS.formClass}>
                 <div className={FormCSS.inputsClass}>
                     <label>Username</label>
-                    <input/>
+                    <input
+                        type= "text"
+                        name="Username"
+                        onChange={handleChange}
+                        placeholder="Username..."
+                    />
                     <label>Password</label>
                     <input/>
                     <label>Email</label>
