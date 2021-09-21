2e0586358cb6abfa50ea414c7cb9533db8cd5620
steven-rr <steverelativity@gmail.com>
2021-09-20 21:19:05 -0400

    adding react router, and sign up page.

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
 import Home from "./pages/Home"
+import Form from "./pages/Form"
+
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <Home></Home>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/" exact component = {Home} />
+          <Route path="/form" component = {Form} />
+
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ client/src/pages/Form.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import FormCSS from "./Form.module.css"
+
+const Form = () => {
+    return (
+        <div className={FormCSS.formContainer}>
+            <div className= {FormCSS.textStyle}>Welcome to Sign Up, Steven! </div>
+
+        </div>
+    )
+}
+
+export default Form
