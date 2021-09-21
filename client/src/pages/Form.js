e4a533f2d2b80231cddf709a0004016975127360
steven-rr <steverelativity@gmail.com>
2021-09-20 21:52:37 -0400

    adding sign up form to client.

--- client/src/pages/Form.js
+++ client/src/pages/Form.js
@@ -4,8 +4,20 @@ import FormCSS from "./Form.module.css"
 const Form = () => {
     return (
         <div className={FormCSS.formContainer}>
-            <div className= {FormCSS.textStyle}>Welcome to Sign Up, Steven! </div>
-
+            <div className= {FormCSS.textStyle}>Create an Account, Steven! </div>
+            <form className= {FormCSS.formClass}>
+                <div className={FormCSS.inputsClass}>
+                    <label>Username</label>
+                    <input/>
+                    <label>Password</label>
+                    <input/>
+                    <label>Email</label>
+                    <input/>
+                </div>
+                <div>
+                    <button>Create Account</button>
+                </div>
+            </form>
         </div>
     )
 }
