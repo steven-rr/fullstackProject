49d9924d98bf26bdd5c1cc371448007a5d4eb788
steven-rr <steverelativity@gmail.com>
2021-09-24 01:08:52 -0400

    login and sign up still in progress. on error handling.

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Home from "./pages/Home"
 import Form from "./pages/Form"
+import Login from "./pages/Login"
 import Posts from "./pages/Posts"
 
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" exact component = {Home} />
           <Route path="/form" component = {Form} />
           <Route path="/blog" component = {Posts} />
+          <Route path="/login" component = {Login} />
         </Switch>
       </div>
     </Router>
