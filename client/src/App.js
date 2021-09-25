5a5040e8cb103dc3f3c3ac88763c4c4afd5fe68a
steven-rr <steverelativity@gmail.com>
2021-09-24 20:09:24 -0400

    adding in navbar for routing

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,22 @@ import Home from "./pages/Home"
 import Form from "./pages/Form"
 import Login from "./pages/Login"
 import Posts from "./pages/Posts"
-
+import Navbar from "./components/Navbar"
+import React, {useState, useEffect} from 'react'
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
 function App() {
+  // reset any relevant state when clicking a link in nav bar.
+  const [value, setValue]=  useState(0);
+  const submitHandler = e =>
+  {
+    setValue(currentValue=> currentValue+1);
+  }
+
   return (
     <Router>
       <div className="App">
+        <Navbar onClick ={submitHandler}></Navbar>
         <Switch>
           <Route path="/" exact component = {Home} />
           <Route path="/form" component = {Form} />
