9de126a1c6b6761d36295d61c35df64b985407de
steven-rr <steverelativity@gmail.com>
2021-09-25 15:13:39 -0400

    adding in .env vars for dev and production

--- server/config/config.js
+++ server/config/config.js
@@ -2,11 +2,11 @@ require('dotenv').config()
 
 module.exports = {
   "development": {
-    "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_DATABASE,
-    "host": process.env.DB_HOST,
-    "port": process.env.DB_PORT,
+    "username": process.env.DB_USERNAME_DEV,
+    "password": process.env.DB_PASSWORD_DEV,
+    "database": process.env.DB_DATABASE_DEV,
+    "host": process.env.DB_HOST_DEV,
+    "port": process.env.DB_PORT_DEV,
     "dialect": "mysql"
   },
   "test": {
@@ -17,10 +17,10 @@ module.exports = {
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "username": process.env.DB_USERNAME_PRODUCTION,
+    "password": process.env.DB_PASSWORD_PRODUCTION,
+    "database": process.env.DB_DATABASE_PRODUCTION,
+    "host": process.env.DB_HOST_PRODUCTION,
     "dialect": "mysql"
   }
 }
