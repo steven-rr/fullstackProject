d5403599ca98818490734edd10bf89e513ec3842
steven-rr <steverelativity@gmail.com>
2021-09-27 05:10:38 -0400

    toggle between previous and upcoming

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -5,35 +5,64 @@ import axios from   "axios"
 const Home = () => {
 
     // store launch data in these variables.
-    const [launchData, setLaunchData] = useState([])
-
+    const [launchDataPrevious, setLaunchDataPrevious] = useState([])
+    const [launchDataUpcoming, setLaunchDataUpcoming] = useState([])
+    const [futureFlag, setFutureFlag]                 = useState([ true])
     // on render, get launch data from backend and display for the user.
     useEffect( async () => {
                 await axios
-                        .get("/api/launches")
+                        .get("/api/launches/previous")
                         .then( (response) =>{
-                            setLaunchData(response.data);
+                            setLaunchDataPrevious(response.data);
                         })
                         .catch( (err) => {
                             console.log("ERROR in Home.js: ", err)
                         })
+                await axios 
+                        .get("/api/launches/upcoming")
+                        .then( (response) =>{
+                            setLaunchDataUpcoming(response.data);
+                        })
+                        .catch( (err) => {
+                            console.log("ERROR in Home.js: ", err)
+                        }) 
     }, []);
+
+    const toggleFutureFlag=  () => {
+        setFutureFlag(!futureFlag)
+    } 
 
 
     return (
         <div className={HomeCSS.homeContainer}>
             <div className= {HomeCSS.textStyle}>Welcome to your server, Steven! </div>
-            {launchData.map((value, key) =>{
-                return (
-                    <div className={HomeCSS.launchItemContainer} key = {key}> 
-                        <div>{value.title} </div>
-                        <div>{value.description} </div>
-                        <img className={HomeCSS.imgContainer} src={value.imgURL}/>
-                        <div > <a className={HomeCSS.buttonClass} href ={value.vidURL}> Watch Video</a> </div>
-                    </div>
-                    
-                )
-            })}
+            <div className={HomeCSS.buttonClass} onClick={toggleFutureFlag}> Button</div>
+            <div className={`${futureFlag ? HomeCSS.deactivate: ''}`}> 
+                {launchDataPrevious.map((value, key) =>{
+                    return (
+                        <div className={HomeCSS.launchItemContainer} key = {key}> 
+                            <div>{value.title} </div>
+                            <div>{value.description} </div>
+                            <img className={HomeCSS.imgContainer} src={value.imgURL}/>
+                            <div > <a className={HomeCSS.buttonClass} href ={value.vidURL}> Watch Video</a> </div>
+                        </div>
+                        
+                    )
+                })}
+            </div>
+            <div className={`${futureFlag ? '': HomeCSS.deactivate}`}> 
+                {launchDataUpcoming.map((value, key) =>{
+                    return (
+                        <div className={HomeCSS.launchItemContainer} key = {key}> 
+                            <div>{value.title} </div>
+                            <div>{value.description} </div>
+                            <img className={HomeCSS.imgContainer} src={value.imgURL}/>
+                            <div > <a className={HomeCSS.buttonClass} href ={value.vidURL}> Watch Video</a> </div>
+                        </div>
+                        
+                    )
+                })}
+            </div>
         </div>
     )
 }
--- server/routes/launches.js
+++ server/routes/launches.js
@@ -2,14 +2,23 @@ const express= require('express');
 const fetch = require("node-fetch")
 const router = express.Router();
 const {LaunchesPrevious}= require('../models');
+const {LaunchesUpcoming}= require('../models');
 
 
 // get launch info for display. 
-router.get('/', async (request, response) => {
+router.get('/previous', async (request, response) => {
     
     const launchData = await LaunchesPrevious.findAll()
     response.json(launchData)
 })
+
+// get launch info for display. 
+router.get('/upcoming', async (request, response) => {
+    
+    const launchData = await LaunchesUpcoming.findAll()
+    response.json(launchData)
+})
+
 
 module.exports = router;
 
