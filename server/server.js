46a33f8cde4caba4976a3bea33fa70a82dfe69ae
steven-rr <steverelativity@gmail.com>
2021-09-30 01:32:36 -0400

    server rendering update

--- server/server.js
+++ server/server.js
@@ -52,7 +52,7 @@ if(process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join('..','client','build')));
 
     app.get('*', (request, response) => {
-        response.sendFile(path.resolve(__dirname,'..','client','build','index.html'));
+        response.sendFile(path.resolve(__dirname,'client','build','index.html'));
     })
 }
 // listen.
