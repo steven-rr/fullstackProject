f29c28aadafdeb75facb8bb1a11506c0cbb6450c
steven-rr <steverelativity@gmail.com>
2021-10-11 03:12:45 -0400

    add comments for reply thread functionality.

--- client/src/components/Comment.js
+++ client/src/components/Comment.js
@@ -5,21 +5,22 @@ import {  Link} from 'react-router-dom'
 // specifies max level to iterate over.
 const REPLY_THREAD_WIDTH = 1;
 
-// recursive component. takes in individual comment, and comments array.
+// recursive component. takes in individual comment, and comments array. 
+// basic algo: if not in the max level yet, keep recursively generating comments. when in max level, if children, link to continue thread.
 const Comment = ({comment, comments, postID, MIN_LEVEL}) => {
     console.log("MIN_LEVEL: ", MIN_LEVEL) 
     const MAX_LEVEL = MIN_LEVEL + REPLY_THREAD_WIDTH
     console.log("MAX_LEVEL: ", MAX_LEVEL);
 
-    // renders child comments recursively. base case is when i hit the max level.
+    // render children recursively until i hit max level. base case is when i hit the max level.
     const nestedComments =  comments.map((commentChild, key) =>{ 
         if(commentChild.parentId === comment.id)
         {
             console.log("commentChildId, commentChildLevel: ", commentChild.id, commentChild.level)
-            // if children will be in max level, stop recursion. else, continue recursion.
+            // if children are in max level, render the children and stop recursion. else, continue recursion.
             if(commentChild.level >= MAX_LEVEL)
             {
-                // iterate thru array. if comment has children, continue the thread.
+                // iterate thru comments. if children at the max level have more children, include link to continue the thread.
                 for(let i =0; i<comments.length; i++)
                 {
                     if(comments[i].parentId == commentChild.id)
@@ -33,7 +34,7 @@ const Comment = ({comment, comments, postID, MIN_LEVEL}) => {
                         )
                     }
                 }
-                // else, no need to continue the thread.
+                // else, children have no more children. no need to continue the thread.
                 return(
                     <div  className = {CommentCSS.commentBodyContainer} >
                         <div className ={CommentCSS.commentText}> {commentChild.contentText} </div>
@@ -42,7 +43,7 @@ const Comment = ({comment, comments, postID, MIN_LEVEL}) => {
                 )
                     
             }
-            else
+            else // continue recursion if not at max level.
             {
                 return (
                     <Comment 
