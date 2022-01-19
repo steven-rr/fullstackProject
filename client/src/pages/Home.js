f11a880d894f875159421a206b6b6aced52d2bf3
steven-rr <steverelativity@gmail.com>
2022-01-19 12:44:36 -0500

    beginning to add real time clocks

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -9,6 +9,7 @@ const Home = () => {
     const [launchDataPrevious, setLaunchDataPrevious] = useState([])
     const [launchDataUpcoming, setLaunchDataUpcoming] = useState([])
     const [futureFlag, setFutureFlag]                 = useState([ true])
+    const [todayTime, setTodayTime]                   = useState([new Date()])
     //on render, get launch data from backend and display for the user.
     useEffect( async () => {
                 await axios
@@ -27,13 +28,44 @@ const Home = () => {
                         .catch( (err) => {
                             console.log("ERROR in Home.js: ", err)
                         }) 
+                console.log("ran first useeffect.")
     }, []);
+    useEffect ( () => {
+                let time = setInterval( () => {
+                    setTodayTime(new Date())
+                }, 1000)
+                console.log("ran second useeffect)")
+    }, [])
 
     const toggleFutureFlag=  () => {
         setFutureFlag(!futureFlag)
     } 
 
+    const calcDeltaTime = (dateMission) => {
+        // calc seconds difference first.
+        var delta = (dateMission - todayTime)/1000
+        // defining parameters
+        const secondsInADay = 60*60*24
+        const secondsInAnHour = 60*60
+        const secondsInAMinute = 60
+        // calc days difference        
+        const daysLeft = Math.floor(delta/secondsInADay)
+        delta = delta - daysLeft*secondsInADay 
 
+        // calc hours difference
+        const hoursLeft= Math.floor(delta/secondsInAnHour)
+        delta = delta - hoursLeft*secondsInAnHour
+
+        // calc minutes difference
+        const minutesLeft = Math.floor(delta/secondsInAMinute)
+        delta = delta - minutesLeft*secondsInAMinute
+
+        // calc seconds difference
+        const secondsLeft = Math.floor(delta)
+
+        const timeLeft ={daysLeft: daysLeft, hoursLeft: hoursLeft, minutesLeft: minutesLeft, secondsLeft: secondsLeft}
+        return timeLeft
+    }
     return (
         <div className={HomeCSS.homeContainer}>
             <div className= {HomeCSS.textStyle}> {futureFlag ? "Upcoming": "Previous"}</div>
@@ -42,6 +74,7 @@ const Home = () => {
             <div className={HomeCSS.buttonClass}> maybe filter by country</div>
             <div className={`${futureFlag ? HomeCSS.deactivate: ''}`}> 
                 {launchDataPrevious.map((value, key) =>{
+                    
                     if(value.imgURL)
                     {
                         return (
@@ -95,6 +128,10 @@ const Home = () => {
             </div> 
             <div className={`${futureFlag ? '': HomeCSS.deactivate}`}> 
                 {launchDataUpcoming.map((value, key) =>{
+                    var date1 = new Date(value.launchDate) 
+                    var timeLeft = calcDeltaTime(date1)
+                    console.log("OUTPUT, CHECK THIS!!: ", date1)
+                    console.log("difference: ", timeLeft )
                     if(value.imgURL)
                     {
                         return (
@@ -112,6 +149,7 @@ const Home = () => {
                                     }
                                     <div className={`${ (value.padName == "Unknown Pad") ? HomeCSS.deactivate: ""}`}>{value.padName}</div>
                                     <div>{value.description} </div>
+                                    <div> {`${timeLeft.daysLeft} : ${timeLeft.hoursLeft} : ${timeLeft.minutesLeft} : ${timeLeft.secondsLeft}`} </div>
                                     <div> {value.launchDate}</div>
                                     <div className={HomeCSS.buttnContainer}> 
                                         {(value.vidURL == null) ? "":(<a className={HomeCSS.buttonClass} href ={value.vidURL}> Watch Video</a> )}
