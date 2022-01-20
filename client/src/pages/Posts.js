3da5befdb83bcd09d7479bc3df5e40d9536c2bf4
steven-rr <steverelativity@gmail.com>
2022-01-20 05:06:46 -0500

    revamp design ! fix up posts styling.

--- client/src/pages/Posts.js
+++ client/src/pages/Posts.js
@@ -7,6 +7,7 @@ import {AuthContext} from "../App"
 const Posts = () => {
     // grabbing setAuthState.
     const {authState, setAuthState} = useContext(AuthContext)
+    const [todayTime, setTodayTime] = useState(new Date())
 
     // instantiate history.
     const history = useHistory();
@@ -22,6 +23,7 @@ const Posts = () => {
             setPostData(response.data);
         })
     }, []);
+
 
     const handleOnClickComments = () => {
         console.log("button clicked!");
@@ -74,20 +76,85 @@ const Posts = () => {
                 console.log(err);
             })
     }
-    
+    // calc time posted to display
+    const postDateToDisplay = (datePosted) => {
+        //output 
+        let result; 
+        // parameters
+        const secsInYear = 60*60*24*365;
+        const secsInMonth = 60*60*24*30;
+
+
+        // check first if post divisible by years:
+        console.log("todayTime: ",todayTime)
+        console.log("datePosted: ", datePosted)
+        let yearDiff = todayTime.getFullYear() - datePosted.getFullYear()
+        let monthDiff = todayTime.getMonth() - datePosted.getMonth()
+        let daysDiff = todayTime.getDate() - datePosted.getDate()
+        let hoursDiff = todayTime.getHours() - datePosted.getHours()
+        let minutesDiff = todayTime.getMinutes() - datePosted.getMinutes()
+        let secondsDiff = Math.floor(todayTime.getSeconds() - datePosted.getSeconds())
+        console.log(yearDiff)
+        console.log(monthDiff)
+        console.log(daysDiff)
+        console.log(secondsDiff)
+        if(yearDiff > 0)
+        {
+            result = `${yearDiff} years ago`
+        }
+        else if(monthDiff >0)
+        {
+            result = `${monthDiff} months ago`
+        
+        }
+        else if (daysDiff >0)
+        {
+            result = `${daysDiff} days ago`
+            
+        }
+        else if (hoursDiff >0)
+        {
+            result = `${hoursDiff} hours ago`
+            
+        }
+        else if (minutesDiff >0)
+        {
+            result = `${minutesDiff} minutes ago`
+            
+        }
+        else if(secondsDiff > 0)
+        {
+            result = `${secondsDiff} seconds ago`
+        }
+        else
+        {
+            result = "0 seconds ago"
+        }
+        
+        
+        return result
+        
+        
+    }
     return (
         <div className={PostsCSS.postsPageContainer}>
-            <div className= {PostsCSS.textStyle}>Steven, check out these posts. </div>
-            {authState.authStatus ? (<><Link to="/createpost">Create Post</Link> </>) : <Link to="/login">Create Post</Link>}
+            {authState.authStatus ? (<><Link className={`${PostsCSS.buttonClass} ${PostsCSS.createPostClass}`} to="/createpost">Create Post</Link> </>) : <Link to="/login">Create Post</Link>}
             
             <div className={PostsCSS.postsBodyContainer}>
                 {postData.map((value, key) =>{
+                    let str = value.createdAt;
+
+                    console.log("createdAt: ", str.substring(0, str.length - 5))
+                    var datePosted= new Date(str)
+                    var dateStringPosted = postDateToDisplay(datePosted)
+
                     return (
                         <div className={PostsCSS.postContainer} key = {key}>
                             <Link  className={PostsCSS.postLinkClass}  to={`/blog/${value.id}`}> 
+                                <div className = {PostsCSS.postAuthor}>{`Posted by ${value.username} ${dateStringPosted}`} </div>
                                 <div className = {PostsCSS.postTitle}>{value.title} </div>
                                 <div className = {PostsCSS.postContent}>{value.contentText} </div>
-                                <div className = {PostsCSS.postContent}>{value.username} </div>
+
                             </Link>
                             <div className={PostsCSS.buttonListClass}> 
                                 <Link to = {`/blog/${value.id}`} className= {PostsCSS.buttonClass} > comments </Link>
